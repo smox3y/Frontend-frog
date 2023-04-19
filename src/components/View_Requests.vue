0f9cf6a104b1b28aa5fa8131b969c718f6e373bc
nagato0519 <nagasushi0519@gmail.com>
2023-04-19 12:48:22 -0500

    API call for View

--- src/components/View_Requests.vue
+++ src/components/View_Requests.vue
@@ -1,86 +1,70 @@
 <template>
-    <div class="view-students">
-        <h1>Request</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Account Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="student in students" :key="student.idNumber">
-            <td>{{ student.idNumber }}</td>
-            <td>{{ student.firstName }}</td>
-            <td>{{ student.lastName }}</td>
-            <td>{{ student.email }}</td>
-            <td>{{ student.accountStatus ? 'Active' : 'Inactive' }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        students: [
-          {
-            idNumber: '001',
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'john.doe@example.com',
-            accountStatus: true,
-          },
-          {
-            idNumber: '002',
-            firstName: 'Jane',
-            lastName: 'Doe',
-            email: 'jane.doe@example.com',
-            accountStatus: false,
-          },
-          {
-            idNumber: '003',
-            firstName: 'Alice',
-            lastName: 'Johnson',
-            email: 'alice.johnson@example.com',
-            accountStatus: true,
-          },
-        ],
-      };
-    },
-    // Remove created() for now, uncomment when ready to use API
-    async created() {
-      const response = await fetch('/api/students');
-      this.students = await response.json();
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .view-students {
-    padding: 1rem;
-    margin-left: 60px;
+  <div>
+    <h1>Requests</h1>
+    <table class="request-table">
+      <thead>
+        <tr>
+          <th>Request ID</th>
+          <th>Requester Name</th>
+          <th>Email</th>
+          <th>Title</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="request in requests" :key="request.requestId">
+          <td>{{ request.requestId }}</td>
+          <td>{{ request.reqFirstName }} {{ request.reqLastName }}</td>
+          <td>{{ request.reqEmail }}</td>
+          <td>{{ request.title }}</td>
+          <td>{{ request.status }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+export default {
+  setup() {
+    const requests = ref([])
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/v1/appearances')
+        requests.value = response.data.data
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    onMounted(fetchData)
+
+    return {
+      requests
+    }
   }
-  
-  table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-  
-  th,
-  td {
-    padding: 0.5rem;
-    text-align: left;
-    border: 1px solid #ccc;
-  }
-  
-  thead {
-    background-color: #4d1979;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.request-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 60px;
+}
+
+.request-table th,
+.request-table td {
+  padding: 10px;
+  border: 1px solid #ccc;
+  text-align: left;
+}
+
+.request-table thead {
+  background-color: #4d1979;
+}
+</style>
--- src/components/View_Students.vue
+++ src/components/View_Students.vue
@@ -1,23 +1,19 @@
 <template>
-  <div class="view-students">
-    <h1>Students</h1>
-    <table>
+  <div>
+      <h1>Students</h1>
+    <table class="student-table">
       <thead>
         <tr>
           <th>ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
+          <th>Name</th>
           <th>Email</th>
-          <th>Account Status</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="student in students" :key="student.idNumber">
-          <td>{{ student.idNumber }}</td>
-          <td>{{ student.firstName }}</td>
-          <td>{{ student.lastName }}</td>
+        <tr v-for="student in students" :key="student.id">
+          <td>{{ student.id }}</td>
+          <td>{{ student.firstName }} {{ student.lastName }}</td>
           <td>{{ student.email }}</td>
-          <td>{{ student.accountStatus ? 'Active' : 'Inactive' }}</td>
         </tr>
       </tbody>
     </table>
@@ -25,61 +21,47 @@
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
 export default {
-  data() {
+  setup() {
+    const students = ref([])
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/v1/users')
+        students.value = response.data.data
+        console.log(students.value)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    onMounted(fetchData)
+
     return {
-      students: [
-        {
-          idNumber: '001',
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'john.doe@example.com',
-          accountStatus: true,
-        },
-        {
-          idNumber: '002',
-          firstName: 'Jane',
-          lastName: 'Doe',
-          email: 'jane.doe@example.com',
-          accountStatus: false,
-        },
-        {
-          idNumber: '003',
-          firstName: 'Alice',
-          lastName: 'Johnson',
-          email: 'alice.johnson@example.com',
-          accountStatus: true,
-        },
-      ],
-    };
-  },
-  // Remove created() for now, uncomment when ready to use API
-  async created() {
-    const response = await fetch('/api/students');
-    this.students = await response.json();
-  },
-};
+      students
+    }
+  }
+}
 </script>
 
 <style scoped>
-.view-students {
-  padding: 1rem;
+.student-table {
+  width: 100%;
+  border-collapse: collapse;
   margin-left: 60px;
 }
 
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-th,
-td {
-  padding: 0.5rem;
-  text-align: left;
+.student-table th,
+.student-table td {
+  padding: 10px;
   border: 1px solid #ccc;
+  text-align: left;
 }
 
-thead {
+.student-table thead {
   background-color: #4d1979;
 }
 </style>
